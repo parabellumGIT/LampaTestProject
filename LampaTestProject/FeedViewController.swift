683f298856d69181732bb6c4feba06325c6bc53e
parabellum199316 <parabellum199316@gmail.com>
2017-11-09 09:23:48 +0200

    InitCommit

---
+++ LampaTestProject/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  LampaTestProject
+//
+//  Created by ParaBellum on 11/9/17.
+//  Copyright © 2017 ParaBellum. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+ 
+
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LampaTestProject' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LampaTestProject
+pod 'ImageSlideshow', '~> 1.4.1'
+pod 'AMScrollingNavbar', '~> 4.0.4'
+pod 'MBProgressHUD', '~> 1.0.0'
+pod 'SDWebImage', '~> 4.1.0'
+
+end

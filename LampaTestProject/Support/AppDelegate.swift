3fcf2bfe68eec5a43ee53705f0423680e853ee20
parabellum199316 <parabellum199316@gmail.com>
2017-11-10 02:56:20 +0200

    URLCache

--- LampaTestProject/Support/AppDelegate.swift
+++ LampaTestProject/Support/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let urlCache = URLCache(memoryCapacity: 500 * 1024 * 1024, diskCapacity: 500 * 1024 * 1024, diskPath: nil)
+        URLCache.shared = urlCache
         return true
     }
 
